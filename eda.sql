65fcaf0815c463d5af51936b1502a66a07e00b91
Tze Min <tzemin.koay@gmail.com>
2022-07-08 14:11:04 +0800

    added script for eda

---
+++ eda.sql
@@ -0,0 +1,114 @@
+-- select data of interest
+SELECT
+	location, date, total_cases, new_cases, total_deaths, population
+FROM Coronavirus.dbo.Death
+ORDER BY 1, 2
+
+-- total cases vs total deaths: likelihood of dying if you contract Covid in Singapore
+SELECT
+	location, date, total_cases, total_deaths
+	, CAST(total_deaths AS FLOAT) * 100 / CAST(total_cases AS FLOAT) AS death_rate
+FROM Coronavirus.dbo.Death
+WHERE location = 'Singapore'
+ORDER BY 1, 2
+
+-- total cases vs population: percentage of country's population that caught Covid
+SELECT
+	location, date, total_cases, population
+	, CAST(total_cases AS FLOAT) * 100 / CAST(population AS FLOAT) AS case_rate
+FROM Coronavirus.dbo.Death
+WHERE location = 'Singapore'
+ORDER BY 1, 2
+
+-- countries with the highest cases per population: highest infection rate per country
+SELECT
+	location, population
+	, MAX(total_cases) AS highest_cases
+	, CAST(MAX(total_cases) AS FLOAT) * 100 / CAST(population AS FLOAT) AS highest_case_rate
+FROM Coronavirus.dbo.Death
+GROUP BY location, population
+ORDER BY highest_case_rate DESC
+
+-- countries with the highest death count: highest death counts per country
+SELECT
+	location, population
+	, MAX(total_deaths) AS highest_deaths
+FROM Coronavirus.dbo.Death
+WHERE continent IS NOT NULL
+GROUP BY location, population
+ORDER BY highest_deaths DESC
+
+-- continents with the highest death count: highest death counts per continent (version 1)
+SELECT
+	location
+	, MAX(total_deaths) AS highest_deaths
+FROM Coronavirus.dbo.Death
+WHERE continent IS NULL
+GROUP BY location
+ORDER BY highest_deaths DESC
+
+-- continents with the highest death count: highest death counts per continent (version 2, strangely different from version 1)
+SELECT
+	continent
+	, MAX(total_deaths) AS highest_deaths
+FROM Coronavirus.dbo.Death
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY highest_deaths DESC
+
+--global numbers
+SELECT 
+	SUM(new_cases) AS total_cases
+	, SUM(new_deaths) AS total_deaths
+	, SUM(CAST(new_deaths AS FLOAT)) / SUM(CAST(new_cases AS FLOAT)) AS death_rate
+FROM Coronavirus.dbo.Death
+WHERE continent IS NOT NULL
+
+--joining deaths and vaccinations
+SELECT
+	d.continent, d.location, d.date, d.population, v.new_vaccinations
+	, SUM(v.new_vaccinations) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_vaccinated -- can't further transform this column
+FROM Coronavirus.dbo.Death d
+JOIN Coronavirus.dbo.Vaccination v
+	ON d.location = v.location
+	AND d.date = v.date
+WHERE d.continent IS NOT NULL
+ORDER BY 2, 3
+
+--therefore, create CTE
+WITH 
+	pop_vs_vac (continent, location, date, population, new_vaccinations, rolling_vaccinated)
+AS (
+	SELECT
+		d.continent, d.location, d.date, d.population, v.new_vaccinations
+		, SUM(v.new_vaccinations) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_vaccinated
+	FROM Coronavirus.dbo.Death d
+	JOIN Coronavirus.dbo.Vaccination v
+		ON d.location = v.location
+		AND d.date = v.date
+	WHERE d.continent IS NOT NULL
+	AND d.location = 'Singapore'
+)
+SELECT 
+	*
+	, CAST(rolling_vaccinated AS FLOAT) * 100 / CAST(population AS FLOAT) AS rolling_vaccinated_rate
+FROM pop_vs_vac
+
+GO
+
+-- create view
+CREATE VIEW PopulationVaccinated 
+AS
+	SELECT
+		d.continent, d.location, d.date, d.population, v.new_vaccinations
+		, SUM(v.new_vaccinations) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rolling_vaccinated
+	FROM Coronavirus.dbo.Death d
+	JOIN Coronavirus.dbo.Vaccination v
+		ON d.location = v.location
+		AND d.date = v.date
+	WHERE d.continent IS NOT NULL
+GO
+
+SELECT 
+	*
+FROM PopulationVaccinated
